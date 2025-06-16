53a04216f698a1a604935ca22e43565009c0d295
Ron Hines <runamonk@gmail.com>
2025-06-16 10:27:46 +0100

    feat: paint last character red in column when column is being cleared. refactor and consolidate code.

--- Column.cs
+++ Column.cs
@@ -7,12 +7,13 @@ namespace NotTheMatrix
 {
     public class Column
     {
-        private int speed;
+        private int speed = 0;
         private int column = 0;
         private int maxrows = 0;
-        private int height;
-        private int rows;
-        private int iteration;
+        private int height = 0;
+        private int rows = 0;
+        private int iteration = 0;
+        private string lastCharacter = string.Empty;
 
         [DllImport("User32.dll")]
         public static extern IntPtr GetDC(IntPtr hwnd);
@@ -27,35 +28,6 @@ namespace NotTheMatrix
             SetRandomSpeed();
         }
 
-        private string GetRandomCharacter()
-        {
-            Random random = new Random();
-            return Constants.ALLOWED_CHARS[random.Next(Constants.ALLOWED_CHARS.Length)].ToString();
-        }
-        private void SetRandomSpeed()
-        {
-            speed = (new Random().Next(100, 2000));
-        }
-        public void Clear()
-        {
-            SetRandomSpeed();
-            rows = 0;
-            try
-            {
-                IntPtr hdc = GetDC(IntPtr.Zero);
-                if (hdc != IntPtr.Zero)
-                {
-                    using (Graphics g = Graphics.FromHdc(hdc))
-                    {
-                        g.FillRectangle(new SolidBrush(Constants.BACKGROUND_COLOR), new Rectangle(column * Constants.DEFAULT_SIZE, 0, Constants.DEFAULT_SIZE, height));
-                    }
-                    ReleaseDC(IntPtr.Zero, hdc);
-                    iteration++;
-                }
-            }
-            catch {}
-        }
-
         async Task doTheThings()
         {
             iteration = 0;
@@ -63,42 +35,79 @@ namespace NotTheMatrix
             {
                 // clear if column is full or random chance occurs
                 if (rows >= maxrows || new Random().Next(0, 1000) < 5)
-                {
                     Clear();
-                }
-                else
-                {
-                    try
-                    {
-                        IntPtr hdc = GetDC(IntPtr.Zero);
-                        if (hdc != IntPtr.Zero)
-                        {
-                            using (Graphics g = Graphics.FromHdc(hdc))
-                            {
-                                g.DrawString(GetRandomCharacter(), Constants.FONT, new SolidBrush(Constants.FOREGROUND_COLOR), column * Constants.DEFAULT_SIZE, rows * Constants.DEFAULT_SIZE);
-                            }
-                            ReleaseDC(IntPtr.Zero, hdc);
-                            iteration++;
-                        }
-                    }
-                    catch {}
-                    rows++;
-                }
+
+                PaintCharacter(GetRandomCharacter(), rows, Constants.FOREGROUND_COLOR);
+
                 if (iteration >= 100)
                 {
                     GC.Collect();
                     iteration = 0;
                 }
-                
+
                 await Task.Delay(speed);
             }
         }
 
+        private void Clear()
+        {
+            if (rows == 0)
+                return;
+
+            PaintCharacter(lastCharacter, rows - 1, Constants.CLEAR_COLOR);
+            Thread.Sleep(100);
+            PaintCharacter(Constants.CLEAR_COLUMN, rows, Constants.BACKGROUND_COLOR);
+
+            iteration++;
+            rows = 0;
+            SetRandomSpeed();
+        }
+
+        private string GetRandomCharacter()
+        {
+            Random random = new Random();
+            return Constants.ALLOWED_CHARS[random.Next(Constants.ALLOWED_CHARS.Length)].ToString();
+        }
+
+        private void PaintCharacter(string character, int row, Color color)
+        {
+            try
+            {
+                IntPtr hdc = GetDC(IntPtr.Zero);
+                if (hdc != IntPtr.Zero)
+                {
+                    using (Graphics g = Graphics.FromHdc(hdc))
+                    {
+                        // clear the column.
+                        if (character == Constants.CLEAR_COLUMN)
+                        {
+                            g.FillRectangle(new SolidBrush(Constants.BACKGROUND_COLOR), new Rectangle(column * Constants.DEFAULT_SIZE, 0, Constants.DEFAULT_SIZE, height));
+                        }
+                        else
+                        {
+                            g.DrawString(character, Constants.FONT, new SolidBrush(color), column * Constants.DEFAULT_SIZE, row * Constants.DEFAULT_SIZE);
+                        }
+                    }
+                    ReleaseDC(IntPtr.Zero, hdc);
+                    lastCharacter = character;
+                    rows++;
+                    iteration++;
+                }
+            }
+            catch { }
+        }
+
+        private void SetRandomSpeed()
+        {
+            speed = (new Random().Next(100, 2000));
+        }
+
         public async void Start()
         {
-            // delay the start randomly, this way we get a staggered effect
+            // delay the start randomly, this way we get a staggered effect           
             await Task.Delay(new Random().Next(100, 100000));
             await Task.Run(() => doTheThings());
         }
+
     }
 }
--- Consts.cs
+++ Consts.cs
@@ -10,8 +10,10 @@ namespace NotTheMatrix
     {
         public const string ALLOWED_CHARS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
         public const int DEFAULT_SIZE = 23;
+        public static readonly string CLEAR_COLUMN = string.Empty;
         public static readonly Color BACKGROUND_COLOR = Color.Black;
         public static readonly Color FOREGROUND_COLOR = Color.Lime;
+        public static readonly Color CLEAR_COLOR = Color.Red;
         public static readonly Font FONT = new Font("Cascadia Mono", DEFAULT_SIZE, FontStyle.Bold, GraphicsUnit.Pixel);
     }
 }
